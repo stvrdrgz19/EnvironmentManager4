77f26425d152d21dc444aeb509c16f6e62a6b953
Steve Rodriguez <38195799+stvrdrgz19@users.noreply.github.com>
2022-12-19 08:16:45 -0500

    Db update log (#70)
    * moved ErrorHandling into it's own class file, added methods to detect, get contents of, and delete SalesPad DB Update log files
    
    * cleaned up some code
    
    * touch ups
    
    Co-authored-by: Steve Rodriguez <steve.rodriguez@Cavallo.com>

---
+++ EnvironmentManager4/ErrorHandling.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentManager4
+{
+    public class ErrorHandling
+    {
+        public static void LogException(Exception e, bool dbUpdate = false, string extraMessage = null)
+        {
+            string logFile = Utilities.GetFile("Log.txt");
+            DateTime logTime = DateTime.Now;
+            using (FileStream stream = File.Open(logFile, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter sw = new StreamWriter(stream))
+                {
+                    sw.WriteLine(String.Format("-({0})-------------------------------------------------", logTime));
+                    sw.WriteLine(String.Format("Environment Manager v{0}", Utilities.GetAppVersion()));
+                    sw.WriteLine(String.Format("Exception Message: {0}", e.Message));
+                    sw.WriteLine(String.Format("Exception Type: {0}", e.GetType().ToString()));
+                    sw.WriteLine(String.Format("Exception Source: {0}", e.Source));
+                    sw.WriteLine(String.Format("Exception Target Site: {0}", e.TargetSite));
+                    sw.WriteLine("");
+                    if (!String.IsNullOrEmpty(extraMessage))
+                    {
+                        sw.WriteLine(extraMessage);
+                        sw.WriteLine("");
+                    }
+                    sw.WriteLine("STACK TRACE");
+                    sw.WriteLine(e.StackTrace);
+                    sw.WriteLine("");
+                }
+            }
+        }
+
+        public static void DisplayExceptionMessage(Exception e, bool dbUpdate = false, string extraMessage = null)
+        {
+            ExceptionForm form = new ExceptionForm();
+            ExceptionForm.exception = e;
+            ExceptionForm.extraMessage = extraMessage;
+            form.ShowDialog();
+        }
+
+        public static void LogDatabaseUpdateFailure()
+        {
+            string logFile = Utilities.GetFile("Log.txt");
+            DateTime logTime = DateTime.Now;
+
+            using (FileStream stream = File.Open(logFile, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter sw = new StreamWriter(stream))
+                {
+                    sw.WriteLine(String.Format("-({0})-------------------------------------------------", logTime));
+                    sw.WriteLine(String.Format("Environment Manager v{0}", Utilities.GetAppVersion()));
+                    sw.WriteLine(GetLogContents());
+                }
+            }
+        }
+
+        public static void DisplayDatabaseUpdateFailure()
+        {
+            ExceptionForm form = new ExceptionForm();
+            ExceptionForm.extraMessage = GetLogContents();
+            ExceptionForm.dbUpdateFail = true;
+            form.ShowDialog();
+        }
+
+        public static void DeleteLogFiles()
+        {
+            foreach (string file in Directory.GetFiles(Utilities.GetCurrentDirectory()))
+            {
+                if (file.Contains("pass_log_") || file.Contains("fail_log_"))
+                    File.Delete(file);
+            }
+        }
+
+        public static bool IsThereAFailLog()
+        {
+            return Directory.GetFiles(Utilities.GetCurrentDirectory()).Any(s => s.Contains("fail_log_"));
+
+            //bool tf = false;
+            //foreach (string file in Directory.GetFiles(Utilities.GetCurrentDirectory()))
+            //    if (file.Contains("fail_log_"))
+            //        tf = true;
+            //return tf;
+        }
+
+        private static string GetLogContents()
+        {
+            string logContents = "Could not find fail log.";
+            foreach (string file in Directory.GetFiles(Utilities.GetCurrentDirectory()))
+                if (file.Contains("fail_log_"))
+                    logContents = File.ReadAllText(file);
+            return logContents;
+        }
+    }
+}
--- EnvironmentManager4/Utilities.cs
+++ EnvironmentManager4/Utilities.cs
@@ -21,8 +21,14 @@ namespace EnvironmentManager4
 {
     public class Utilities
     {
-
         //Setup File Video - https://www.youtube.com/watch?v=tiHBwAp_Kz4&t=179s
+
+        public const string debugPath = @"C:\Users\steve.rodriguez\source\repos\EnvironmentManager4\EnvironmentManager4\bin\Debug";
+        public const string localPath = @"C:\Program Files (x86)\Environment Manager";
+
+        /// <summary>
+        /// Current versions of SalesPad that are supported.
+        /// </summary>
         public static List<string> versionList = new List<string>
         {
             "x86",
@@ -32,26 +38,22 @@ namespace EnvironmentManager4
 
         public static bool DevEnvironment()
         {
-            if (Environment.CurrentDirectory == @"C:\Users\steve.rodriguez\source\repos\EnvironmentManager4\EnvironmentManager4\bin\Debug")
-                return true;
-            else
-                return false;
+            return Environment.CurrentDirectory == debugPath ? true : false;
         }
 
         public static string GetFile(string fileName)
         {
-            if (DevEnvironment())
-                return String.Format(@"{0}\Files\{1}", @"C:\Program Files (x86)\Environment Manager", fileName);
-            else
-                return String.Format(@"{0}\Files\{1}", Environment.CurrentDirectory, fileName);
+            return DevEnvironment() ? String.Format(@"{0}\Files\{1}", localPath, fileName) : String.Format(@"{0}\Files\{1}", Environment.CurrentDirectory, fileName);
         }
 
         public static string GetFolder(string folderName)
         {
-            if (DevEnvironment())
-                return String.Format(@"{0}\{1}", @"C:\Program Files (x86)\Environment Manager", folderName);
-            else
-                return String.Format(@"{0}\{1}", Environment.CurrentDirectory, folderName);
+            return DevEnvironment() ? String.Format(@"{0}\{1}", localPath, folderName) : String.Format(@"{0}\{1}", Environment.CurrentDirectory, folderName);
+        }
+
+        public static string GetCurrentDirectory()
+        {
+            return DevEnvironment() ? localPath : Environment.CurrentDirectory;
         }
 
         public static string GetIP(string networkName)
@@ -70,9 +72,8 @@ namespace EnvironmentManager4
                         continue;
 
                     if (network.Name.Contains(networkName))
-                    {
                         return address.Address.ToString();
-                    }
+
                     ip = "NOT CONNECTED";
                 }
             }
@@ -84,18 +85,13 @@ namespace EnvironmentManager4
             int count = lv.Items.Count;
             int maxW = lv.Columns[colIndx].Width;
             if (count > maxRowCount)
-            {
                 lv.Columns[colIndx].Width = maxW - 17;
-            }
         }
 
         public static void ResizeUpdateableListViewColumnWidthForScrollBar(ListView lv, int maxRowCount, int colIndx, int colDefaultWidth)
         {
             int count = lv.Items.Count;
-            if (count > maxRowCount)
-                lv.Columns[colIndx].Width = colDefaultWidth - 17;
-            else
-                lv.Columns[colIndx].Width = colDefaultWidth;
+            lv.Columns[colIndx].Width = count > maxRowCount ? colDefaultWidth - 17 : colDefaultWidth;
         }
 
         public static int GetNthIndex(string s, char t, int n)
@@ -107,9 +103,7 @@ namespace EnvironmentManager4
                 {
                     count++;
                     if (count == n)
-                    {
                         return i + 1;
-                    }
                 }
             }
             return -1;
@@ -151,8 +145,8 @@ namespace EnvironmentManager4
             catch (Exception e)
             {
                 string extraMessage = "Possibly not connected to the SalesPad Network or VPN.";
-                ErrorHandling.DisplayExceptionMessage(e, extraMessage);
-                ErrorHandling.LogException(e, extraMessage);
+                ErrorHandling.DisplayExceptionMessage(e, false, extraMessage);
+                ErrorHandling.LogException(e, false, extraMessage);
                 version = "Unable to Connect";
             }
             return version;
@@ -283,75 +277,18 @@ namespace EnvironmentManager4
         }
     }
 
-    public class ErrorHandling
-    {
-        public static void LogException(Exception e, string extraMessage = null)
-        {
-            string logFile = Utilities.GetFile("Log.txt");
-            DateTime logTime = DateTime.Now;
-            if (!File.Exists(logFile))
-            {
-                using (StreamWriter sw = File.CreateText(logFile))
-                {
-                    sw.WriteLine(String.Format("-({0})-------------------------------------------------", logTime));
-                    sw.WriteLine(String.Format("Environment Manager v{0}", Utilities.GetAppVersion()));
-                    sw.WriteLine(String.Format("Exception Message: {0}", e.Message));
-                    sw.WriteLine(String.Format("Exception Type: {0}",e.GetType().ToString()));
-                    sw.WriteLine(String.Format("Exception Source: {0}",e.Source));
-                    sw.WriteLine(String.Format("Exception Target Site: {0}",e.TargetSite));
-                    sw.WriteLine("");
-                    if (!String.IsNullOrEmpty(extraMessage))
-                    {
-                        sw.WriteLine(extraMessage);
-                        sw.WriteLine("");
-                    }
-                    sw.WriteLine("STACK TRACE");
-                    sw.WriteLine(e.StackTrace);
-                    sw.WriteLine("");
-                }
-            }
-            else
-            {
-                using (StreamWriter sw = File.AppendText(logFile))
-                {
-                    sw.WriteLine(String.Format("-({0})-------------------------------------------------", logTime));
-                    sw.WriteLine(String.Format("Environment Manager v{0}", Utilities.GetAppVersion()));
-                    sw.WriteLine(String.Format("Exception Message: {0}", e.Message));
-                    sw.WriteLine(String.Format("Exception Type: {0}",e.GetType().ToString()));
-                    sw.WriteLine(String.Format("Exception Source: {0}", e.Source));
-                    sw.WriteLine(String.Format("Exception Target Site: {0}", e.TargetSite));
-                    sw.WriteLine("");
-                    if (!String.IsNullOrEmpty(extraMessage))
-                    {
-                        sw.WriteLine(extraMessage);
-                        sw.WriteLine("");
-                    }
-                    sw.WriteLine("STACK TRACE");
-                    sw.WriteLine(e.StackTrace);
-                    sw.WriteLine("");
-                }
-            }
-        }
-
-        public static void DisplayExceptionMessage(Exception e, string extraMessage = null)
-        {
-            ExceptionForm form = new ExceptionForm();
-            ExceptionForm.exception = e;
-            ExceptionForm.extraMessage = extraMessage;
-            form.ShowDialog();
-        }
-    }
-
     public class RegUtilities
     {
+        public const string installFolder = @"Software\Environment Manager\Install";
+
         public static RegistryKey GetInstallSubRegKey(string product)
         {
-            return Registry.CurrentUser.OpenSubKey(String.Format(@"Software\Environment Manager\Install\{0}", product), true);
+            return Registry.CurrentUser.OpenSubKey(String.Format(@"{0}\{1}", installFolder, product), true);
         }
 
         public static void CheckForInstallRegistryEntries()
         {
-            RegistryKey spKey = Registry.CurrentUser.OpenSubKey(String.Format(@"Software\Environment Manager\Install\{0}", Products.SalesPad));
+            RegistryKey spKey = Registry.CurrentUser.OpenSubKey(String.Format(@"{0}\{1}", installFolder, Products.SalesPad));
             if (spKey == null)
                 GenerateInstallOptionEntries();
         }
@@ -360,7 +297,7 @@ namespace EnvironmentManager4
         {
             foreach (string product in Products.ListOfProducts())
             {
-                Registry.CurrentUser.CreateSubKey(String.Format(@"Software\Environment Manager\Install\{0}", product));
+                Registry.CurrentUser.CreateSubKey(String.Format(@"{0}\{1}", installFolder, product));
                 RegistryKey key = GetInstallSubRegKey(product);
                 key.SetValue(RegistryEntries._LaunchAfterInstall, "false");
                 key.SetValue(RegistryEntries._OpenInstallFolder, "false");
